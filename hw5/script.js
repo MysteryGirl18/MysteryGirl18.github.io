408066ff9e7c857c3eed85649dc23dfba0a1b909
Sanjana Gupta <sg6575@nyu.edu>
2023-11-06 00:21:38 -0500

    finished hw5

--- hw5/script.js
+++ hw5/script.js
@@ -1,17 +1,28 @@
 console.log("the Message!");
 
-let theButton = document.getElementById("historyEraser");
+let theButton = document.getElementById("btn");
 let theMain = document.querySelector("main")
 theButton.addEventListener('click',foo);
 
-const canvas = document.querySelector("canvas");
+const canvas = document.querySelector("urCanvas");
 const ctx = canvas.getContext("2d");
 
 ctx.fillStyle = "green";
 ctx.fillRect(10, 10, 150, 100);
+ctx.font = "30px Arial";
+ctx.fillText("hiiiiiiiii", 10, 50);
 
 function foo(){
     console.log("you clicked!");
-    theMain.style.backgroundColor = "yellow";
-    theMain.textContent = "you clicked!"
+    let p = document.getElementById('post').value
+    theMain.style.backgroundColor = "black";
+    theButton.textContent = p
+
+  
+    ctx.font = "30px Arial";
+    ctx.strokeText(p, 10, 50);
 }
+
+
+// var c = document.getElementById("urCanvas");
+// var ctx = c.getContext("2d");
