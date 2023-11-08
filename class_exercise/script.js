06e9a82f6c630069d370509ac65079b15dd77bef
Sanjana Gupta <sg6575@nyu.edu>
2023-11-08 11:39:32 -0500

    added list js class-exercise code

---
+++ class_exercise/script.js
@@ -0,0 +1,63 @@
+// console.log("theMessage");
+
+// const theButton = document.querySelector("button");
+// const theInput = document.querySelector("input")
+// let theDivider = document.querySelector("div")
+
+// let theUserWords;
+// let theItem;
+// let theTxt;
+
+// theButton.addEventListener('click', makeList)
+
+// function makeList() {
+//     console.log("click!");
+//     theUserWords = theInput.value;
+//     // console.log(theUserWords)
+//     theItem = document.createElement('li')
+//     const theSpan = document.createElement('span');
+//     theTxt = document.createTextNode(theUserWords);
+//     // document.body.appendChild(theItem);
+//     theDivider.appendChild(theItem);
+//     theItem.appendChild(theTxt);
+// }
+
+// document.querySelector("p").addEventListener('click', eraseList)
+
+// function eraseList(){
+//     console.log("erase!");
+//     document.querySelector("div").remove(theItem)
+// }
+
+// ------------------------------------------------------------
+
+console.log("starting script...");
+
+const addButton = document.querySelector("button");
+const inputBox = document.querySelector("input");
+let container = document.querySelector("div");
+
+let theUserWords;
+let theItem;
+let theTxt;
+
+function makeList() {
+    console.log("click!");
+    theUserWords = inputBox.value;
+
+    theItem = document.createElement("li");
+    theTxt = document.createTextNode(theUserWords);
+    theItem.appendChild(theTxt)
+
+    container.appendChild(theItem);     // could also have appended directly to DOM using document.body.appendChild()
+}
+
+addButton.addEventListener('click', makeList)
+
+function eraseListItem(){
+    console.log("commencing erase!");
+    document.querySelector("div").remove(theItem);
+}
+
+document.querySelector("p").addEventListener("click", eraseList);
+
