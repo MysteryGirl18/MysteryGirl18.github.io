4b81655a3ab069ea9ca9ecdd59b96aae0a6ed83a
Sanjana Gupta <sg6575@nyu.edu>
2023-12-15 22:06:40 -0500

    modified script.js code in final_proj that links to animation page

--- final_proj/script.js
+++ final_proj/script.js
@@ -1,5 +1,3 @@
-const noMoreQuestions = 'END_OF_PATH';
-
 const questions = [
     { question: 
         "What does philosophy mean to you?", 
@@ -109,16 +107,219 @@ const questions = [
 ];
 
 // map answers to the next question
-// if answer maps to noMoreQuestions, can end game
-const qMappings = [
-    { answer: 
-        "Is ring greater to it than the sum of its parts?", 
-      question: [
-        "Option 1A", 
-        "Option 1B",
-      ] 
-    },
-];
+// stores if answer maps to question or a link
+// const ansMaps = [
+//     { answer: 
+//       "Something deeply intertwined with daily life", 
+//       mapping: [
+//         True, 
+//         "How does philosophy connect to daily life?",
+//       ] 
+//     },
+//     { answer: 
+//       "Capturing the big picture, something above the level of daily life", 
+//       mapping: [
+//         True, 
+//         "What kinds of questions constitutes the big picture of life to you?",
+//       ] 
+//     },
+//     { answer: 
+//       "How do we, and should we, reason through life? (logic)",
+//       mapping: [
+//         True, 
+//         "How do people use logic to deliver valid philosophical arguments?",
+//       ] 
+//     },
+//     { answer: 
+//       "What is the nature of reality and that which we perceive? (metaphysics)",
+//       mapping: [
+//         True, 
+//         "What is the true nature of reality?",
+//       ] 
+//     },
+//     { answer: 
+//       "How do we assign value in the world? (value theory)",
+//       mapping: [
+//         True, 
+//         "How do we determine our values in relation to…?",
+//       ] 
+//     },
+//     { answer: 
+//       "At what point do we deem ourselves to have knowledge of something (epistemology)?",
+//       mapping: [
+//         True, 
+//         "Our (philosophers') interest in the concept of knowledge is tied to….?",
+//       ] 
+//     },
+//     { answer: 
+//       "Logic is the basis underpinning all of philosophy: I want to learn more about the formal logic methods used to support arguments", 
+//       mapping: [
+//         False, 
+//         "",
+//       ] 
+//     },
+//     { answer: 
+//       "Logic is just a tool, I'm more interested in the subjects people like to debate over",
+//       mapping: [
+//         True, 
+//         "Logic is just a tool, I'm more interested in the subjects people like to debate over…",
+//       ] 
+//     },
+//     { answer: 
+//       "Does the reality we see correspond to the objective reality that exists outside of us? What actually exists? (ontology)",
+//       mapping: [
+//         True, 
+//         "Does the reality we see correspond to the objective reality that exists outside of us? What actually exists?",
+//       ] 
+//     },
+//     { answer: 
+//       "Is reality completely predictable by physics and the sciences, or is the something greater to it than the sum of its parts? (philosophy of science)", 
+//       mapping: [
+//         True, 
+//         "Is reality completely predictable by physics and the sciences, or is the something greater to it than the sum of its parts?",
+//       ] 
+//     },
+//     { answer: 
+//       "Human morality (ethics)",
+//       mapping: [
+//         False, 
+//         "",
+//       ] 
+//     },
+//     { answer: 
+//       "Measuring beauty and appearance, when so much of how we judge the world is tied to how we see and perceive it (aesthetics)",
+//       mapping: [
+//         False, 
+//         "",
+//       ] 
+//     },
+//     { answer: 
+//       "How do/should we define knowledge?",
+//       mapping: [
+//         True, 
+//         "How do we define knowledge?",
+//       ] 
+//     },
+//     { answer: 
+//       "What responsibilities/precautions must people bear in handling and understanding knowledge?",
+//       mapping: [
+//         True, 
+//         "What responsibilities/precautions must people bear in handling and understanding knowledge?",
+//       ] 
+//     },
+//     { answer: 
+//       "I question the physical world",
+//       mapping: [
+//         True, 
+//         "How should I interpret the physical world?",
+//       ] 
+//     },
+//     { answer: 
+//       "I question the abstract",
+//       mapping: [
+//         True, 
+//         "How can I qualify abstract human concepts?",
+//       ] 
+//     },
+//     { answer: 
+//       "Seeing is believing! (empiricism)",
+//       mapping: [
+//         False, 
+//         "",
+//       ]
+//     },
+//     { answer: 
+//       "How can I believe anything I see? (skepticism)",
+//       mapping: [
+//         False, 
+//         "",
+//       ] 
+//     },
+//     { answer: 
+//       "Isn't god supposed to be greater than anything else in the universe, if he exists? (philosophy of religion)",
+//       mapping: [
+//         False, 
+//         "",
+//       ] 
+//     },
+//     { answer: 
+//       "I think science is the key to answering this question! (philosophy of science)",
+//       mapping: [
+//         False, 
+//         "",
+//       ] 
+//     },
+//     { answer: 
+//       "Easy! On the basis of objective truth and reasoning!",
+//       mapping: [
+//         False, 
+//         "",
+//       ] 
+//     },
+//     { answer: 
+//       "I bet philosophers struggle to capture the boundaries of knowledge in a clear-cut way.",
+//       mapping: [
+//         False, 
+//         "",
+//       ] 
+//     },
+//     { answer: 
+//       "Does anything actually exist? (existentialism)",
+//       mapping: [
+//         False,
+//         "",
+//       ]
+//     },
+//     { answer: 
+//       "Can I trust what I see? (empiricism, skepticism)",
+//       mapping: [
+//         False, 
+//         "",
+//       ] 
+//     },
+//     { answer: 
+//       "How do we determine ethical standards?",
+//       mapping: [
+//         False, 
+//         "",
+//       ] 
+//     },
+//     { answer: 
+//       "How do we value the beauty/appearance of things?",
+//       mapping: [
+//         False, 
+//         "",
+//       ] 
+//     },
+//     { answer: 
+//       "People can believe whatever they like privately",
+//       mapping: [
+//         False, 
+//         "",
+//       ] 
+//     },
+//     { answer: 
+//       "People must do their best to justify all their beliefs based off evidence",
+//       mapping: [
+//         True, 
+//         "How do people go about acquiring evidence to justify their beliefs?",
+//       ] 
+//     },
+//     { answer: 
+//       "Through healthy scientific skepticism and the scientific method",
+//       mapping: [
+//         False,
+//         "",
+//       ]
+//     },
+//     { answer: 
+//       "Science vs Pseudoscience- how do we distinguish between the two?",
+//       mapping: [
+//         False,
+//         "",
+//       ]
+//     },
+// ];
 
 let currentQuestion = 0;
 
@@ -167,7 +368,8 @@ function nextQuestion(option) {
         // Display game over or completion message
         // document.getElementById("question-container").innerHTML = "<h2>Game Over!</h2>";
         // window.location.href = '/anime.html'
-        window.location.pathname = '/anime.html'
+        // window.location.pathname = '/anime.html'
+        window.location.href = '/anime.html';
         // document.location.href = newUrl;
     }
 }
